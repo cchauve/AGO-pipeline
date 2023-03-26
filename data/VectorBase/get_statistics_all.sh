ceca63e445e1deb52d37127e1f040ad9a4541472
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2023-03-26 08:14:33 -0700

    VectorBase stats

--- data/VectorBase/get_statistics_all.sh
+++ data/VectorBase/get_statistics_all.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X 1 2 >  statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X 2 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X 1 3 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X 2 3 >> statistics.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X all 1 2 >  statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X all 2 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X all 1 3 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt X all 2 3 >> statistics_all.txt
 
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 2L 2R' 1 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 2L 2R' 2 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 2L 2R' 1 3 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 2L 2R' 2 3 >> statistics.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 2L 2R' 1 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 2L 2R' 2 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 2L 2R' 1 3 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 2L 2R' 2 3 >> statistics_all.txt
 
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '3 3L 3R' 1 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '3 3L 3R' 2 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '3 3L 3R' 1 3 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '3 3L 3R' 2 3 >> statistics.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '3 3L 3R' 1 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '3 3L 3R' 2 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '3 3L 3R' 1 3 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '3 3L 3R' 2 3 >> statistics_all.txt
 
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 1 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 2 2 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 1 3 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 2 3 >> statistics.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 1 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 2 2 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 1 3 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 2 3 >> statistics_all.txt
 
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 3 5 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 4 5 >> statistics.txt
-python ../../scripts/VectorBase_utils.py stats genes_20230311.txt '2 3 2L 2R 3L 3R' 4 4 >> statistics.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 3 5 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 4 5 >> statistics_all.txt
+python ../../scripts/VectorBase_utils.py stats genes_20230311.txt all '2 3 2L 2R 3L 3R' 4 4 >> statistics_all.txt
