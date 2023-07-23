4e4fa5632edd3096578698e0061eac4f9a4bb4f0
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2023-07-23 09:25:48 -0700

    cleaning

--- exp/COUNT/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-java -Xmx2048M -cp ./bin/Count.jar ca.umontreal.iro.evolution.genecontent.ML dev/species_tree.newick dev/family_content.csv \
-    | sed 's/node /node_/g' | sed "s/'//g" > dev/test_ML.out
-java -Xmx2048M -cp ./bin/Count.jar ca.umontreal.iro.evolution.genecontent.Posteriors dev/species_tree.newick dev/family_content.csv dev/test_ML.out \
-    | sed 's/node /node_/g' | sed "s/'//g" > dev/test_Posteriors.out
-java -Xmx2048M -cp ./bin/Count.jar ca.umontreal.iro.evolution.genecontent.AsymmetricWagner dev/species_tree.newick dev/family_content.csv \
-    | sed 's/node /node_/g' | sed "s/'//g" > dev/test_AsymmetricWagner.out
-
