985a758e70fdaca1c367e5f42b0444ca613f602e
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2023-08-02 11:29:33 -0700

    OMA exp_2 increase array size

--- exp/OMA_20230728/exp_2/run_oma2.sh
+++ exp/OMA_20230728/exp_2/run_oma2.sh
@@ -5,8 +5,8 @@ EXP_NAME=exp_2
 TMP_EXP_DIR=${OMA_TMP_DIR}/${EXP_NAME}
 cd ${TMP_EXP_DIR}
 
-sbatch --account=def-chauvec --array=1-100 --time=16:00:00 --mem=8G --output=log/OMA2_%A_%a.out --error=log/OMA2_%A_%a.err --job-name=OMA2 -N1 <<EOF
+sbatch --account=def-chauvec --array=1-200 --time=16:00:00 --mem=8G --output=log/OMA2_%A_%a.out --error=log/OMA2_%A_%a.err --job-name=OMA2 -N1 <<EOF
 #!/bin/sh 
-export NR_PROCESSES=100
+export NR_PROCESSES=200
 ${OMA_PATH}/oma -s
 EOF
