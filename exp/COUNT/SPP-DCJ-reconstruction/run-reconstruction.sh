57e5733fbb1b64205d174b9cfbc183685a814d93
Dany Doerr <daniel.doerr@hhu.de>
2023-12-13 15:16:16 +0100

    added result files for spp-dcj reconstruction

---
+++ exp/COUNT/SPP-DCJ-reconstruction/run-reconstruction.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+COUNT_DATA_DIR=../Anopheles
+RAW_DATA_DIR=../../../data/VectorBase
+SPP_DCJ_BIN=./spp_dcj_v2/scripts
+THREADS=10
+MIN_SIZE=5
+
+echo "produce adjacencies file (adjacencies.txt)"
+$SPP_DCJ_BIN/count2adjacencies.py $RAW_DATA_DIR/species_tree_4.newick $COUNT_DATA_DIR/X_2_AsymmetricWagner.family.csv $RAW_DATA_DIR/families_X_2.txt $RAW_DATA_DIR/gene_orders_X_2.rel.txt > adjacencies.txt 2> adjacencies.log
+
+echo "prepare input files for SPP-DCJ (X_2_AsymmetricWagner.family.clean.tsv, species_tree_4.tsv)"
+cut -f-8 $COUNT_DATA_DIR/X_2_AsymmetricWagner.family.csv > X_2_AsymmetricWagner.family.clean.csv
+$SPP_DCJ_BIN/nwk2tabular.py $RAW_DATA_DIR/species_tree_4.newick > species_tree_4.tsv
+
+echo "run SPP-DCJ (X_2_AsymmetricWagner.family.adjacencies.ilp)"
+$SPP_DCJ_BIN/spp_dcj.py -c X_2_AsymmetricWagner.family.clean.csv -t species_tree_4.tsv adjacencies.txt -m X_2_AsymmetricWagner.family.adjacencies.idmap > X_2_AsymmetricWagner.family.adjacencies.ilp 2> X_2_AsymmetricWagner.family.adjacencies.sppdcj.log
+
+echo "run gurobi (X_2_AsymmetricWagner.family.adjacencies.sol)"
+#gurobi_cl Threadss=$THREADS ResultFile=X_2_AsymmetricWagner.family.adjacencies.sol LogFile=X_2_AsymmetricWagner.family.adjacencies.log X_2_AsymmetricWagner.family.adjacencies.ilp
+
+echo "extract adjacencies (X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.tsv)"
+$SPP_DCJ_BIN/sol2adjacencies.py X_2_AsymmetricWagner.family.adjacencies.sol X_2_AsymmetricWagner.family.adjacencies.idmap > X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.tsv 2> X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.log
+
+echo "produce UniMoG file (X_2_AsymmetricWagner.family.adjacencies.sol.unimog.tsv)"
+$SPP_DCJ_BIN/sol2unimog.py X_2_AsymmetricWagner.family.adjacencies.sol X_2_AsymmetricWagner.family.adjacencies.idmap > X_2_AsymmetricWagner.family.adjacencies.sol.unimog.tsv 2> X_2_AsymmetricWagner.family.adjacencies.sol.unimog.log
+
+echo "visualize reconstructed genomes (X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.pdf), omitting components smaller $MIN_SIZE"
+$SPP_DCJ_BIN/visualize_genomes.py -o $MIN_SIZE X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.tsv 2>&1 > X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.pdf | tee X_2_AsymmetricWagner.family.adjacencies.sol.adjacencies.log
